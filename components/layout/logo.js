224a1aa5ad4de4ded6263d0768513762b4ee565f
philipkogel <philipkogel@yahoo.pl>
2022-02-03 21:41:08 +0100

    Home page basic look, posts components and styles

---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>Philip's NextJS Blog</div>;
+}
+
+export default Logo;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
 import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+
+const dummy_posts = [
+  {
+    slug: "demo-next-js",
+    title: "Demo nextJS post",
+    image: "demo-nextjs-blog.jpg",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server site rendering",
+    date: "2022-02-10",
+  },
+  {
+    slug: "demo2-next-js",
+    title: "Demo nextJS post",
+    image: "demo-nextjs-blog.jpg",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server site rendering",
+    date: "2022-02-10",
+  },
+  {
+    slug: "demo3-next-js",
+    title: "Demo nextJS post",
+    image: "demo-nextjs-blog.jpg",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server site rendering",
+    date: "2022-02-10",
+  },
+  {
+    slug: "demo4-next-js",
+    title: "Demo nextJS post",
+    image: "demo-nextjs-blog.jpg",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server site rendering",
+    date: "2022-02-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={dummy_posts} />
     </Fragment>
   );
 }
